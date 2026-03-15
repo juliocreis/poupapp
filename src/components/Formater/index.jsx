63ff39f502f3b4ac43206572e4c241d954e0f9b4
Julio Cesar <julioreis951@gmail.com>
2026-03-15 20:34:06 -0300

    refactor: create the formatter outside the component

--- src/components/Formater/index.jsx
+++ src/components/Formater/index.jsx
@@ -1,8 +1,9 @@
+const formater = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+
 export function Formater({ value }) {
-  const formater = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
 
   return <>{formater.format(value)}</>;
 }
