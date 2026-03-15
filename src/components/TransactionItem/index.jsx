709b0705f2dff42a5fb263d68528fbc8d734fb5f
Julio Cesar <julioreis951@gmail.com>
2026-03-15 20:54:53 -0300

    feat: changes text color if the transaction value is greater or less than 1

--- src/components/TransactionItem/index.jsx
+++ src/components/TransactionItem/index.jsx
@@ -3,12 +3,13 @@ import styles from './transactionitem.module.css'
 
 export function TransactionItem({ item }) {
   const data = new Date(item.date).toLocaleDateString()
+  const color = item.value > 1 ? styles.item__green : styles.item__orange
 
   return (
     <div className={styles.transaction__item}>
       <div className={styles.info__item}>
-        <p className={styles.item__description}>{item.description}</p>
-        <p className={styles.item__value}><Formater value={item.value}></Formater></p>
+        <p className={`${styles.item__description} ${color}`}>{item.description}</p>
+        <p className={color}><Formater value={item.value}></Formater></p>
       </div>
       <div className={styles.item__date}>{data}</div>
     </div>
