ccf88410c69125e9d952f21d051b93e75e77b2e2
Julio Cesar <julioreis951@gmail.com>
2026-03-16 15:16:42 -0300

    feat: add logo and background color to aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import logo from "./assets/logo.svg"
 
 function App() {
   return (
     <>
-      
+      <div className="bg-neutral-background min-h-screen">
+        <aside>
+            <img src={logo} alt="Logo do Poupapp" />
+        </aside>
+      </div>
     </>
   );
 }
