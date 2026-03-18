b0dc4022063b49b4ca04b9cbc15f3483596d806c
Julio Cesar <julioreis951@gmail.com>
2026-03-17 23:39:11 -0300

    feat: creates the card structure and styles the header

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,63 @@ import { Main } from "./components/Main";
 import { SearchInput } from "./components/SearchInput";
 import { Typography } from "./components/Typography";
 
+import Card from "./components/Cards";
+
 function App() {
   return (
     <div className="bg-neutral-background min-h-screen">
       <Container>
         <Aside />
         <Main>
-          <SearchInput type="text" placeholder="Procure seu dinheiro..." name="q"/>
+          <SearchInput
+            type="text"
+            placeholder="Procure seu dinheiro..."
+            name="q"
+          />
           <div className="flex flex-col gap-2">
-            <Typography variant='h1'>Olá, Júlio!</Typography>
-            <Typography variant='p'>Veja como estão suas finanças hoje.</Typography>
+            <Typography variant="h1">Olá, Júlio!</Typography>
+            <Typography variant="p">
+              Veja como estão suas finanças hoje.
+            </Typography>
           </div>
+
+          <section className="grid grid-cols-2 gap-6">
+            <Card>
+              <Card.Header>
+                Orçamento diário disponível:
+              </Card.Header>
+              <Card.Body>
+                R$200
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header>
+                Progresso da meta financeira
+              </Card.Header>
+              <Card.Body>
+                
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header>
+                Movimentação financeira
+              </Card.Header>
+              <Card.Body>
+                200,00
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header>
+                Minhas contas
+              </Card.Header>
+              <Card.Body>
+                
+              </Card.Body>
+            </Card>
+          </section>
         </Main>
       </Container>
     </div>
---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,28 @@
+export function Card({ children }) {
+    return (
+        <div className="bg-neutral-surface w-[435px] h-[171px] rounded-3xl">
+            {children}
+        </div>
+    )
+}
+
+export function Header({ children }) {
+    return (
+        <div className="bg-neutral-header h-[56px] rounded-t-3xl text-[19px] text-neutral-text font-bold py-4 px-[75px] text-center" >
+            {children}
+        </div>
+    )
+}
+
+export function Body({ children }) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+Card.Header = Header;
+Card.Body = Body
+
+export default Card
