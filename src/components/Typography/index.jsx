e9f0fe535d44f07ca22f4832a9fd39cc62eff8e9
Julio Cesar <julioreis951@gmail.com>
2026-03-17 22:44:42 -0300

    refactor: create an object using the tailwind style

--- src/components/Typography/index.jsx
+++ src/components/Typography/index.jsx
@@ -3,13 +3,17 @@ const TAGS = {
     p: 'p'
 }
 
+const VARIANT_CLASS = {
+    h1: 'text-[31px] leading-[120%] font-bold text-neutral-text',
+    p: 'text-[16px] leading-[120%] font-normal text-neutral-text'
+}
+
 export function Typography({ variant, children }) {
-    const Component = TAGS[variant] || 'p';
-    const variantClass = Component == 'h1' ? 
-        'text-[31px] leading-[120%] font-bold text-neutral-text' : 
-        'text-[16px] leading-[120%] font-normal text-neutral-text'
+    const Component = TAGS[variant] || TAGS.p;
+    const className = VARIANT_CLASS[variant] || VARIANT_CLASS.p
+
     return (
-        <Component className={variantClass}>
+        <Component className={className}>
             {children}
         </Component>
     )
