51ada1af513e47f3c00de8402dc093610afa30eb
Julio Cesar <julioreis951@gmail.com>
2026-03-20 17:23:45 -0300

    change aside text

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -4,7 +4,7 @@ export function Aside() {
   return (
     <aside className="bg-neutral-header max-w-[282px] min-h-screen py-20 px-10 flex flex-col gap-[56px]" >
       <img src={logo} alt="Logo do Poupapp" />
-      <p className="text-neutral-text text-center text-[13px] leading-[120%]">Desenvolvido por Alura. Projeto fictício sem fins comerciais.</p>
+      <p className="text-neutral-text text-center text-[13px] leading-[120%]">Desenvolvido por Júlio Reis.</p>
     </aside>
   );
 }
